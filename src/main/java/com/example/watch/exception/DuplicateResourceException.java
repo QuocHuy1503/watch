67b756ed7c3c873ed7d5ce5f34dca3be2d0fe299
QuocHuy1503 <qnhuy213@gmail.com>
2025-04-25 17:07:21 +0700

    restart project vì db chưa xác định :33

--- src/main/java/com/example/watch/exception/DuplicateResourceException.java
+++
@@ -1,9 +0,0 @@
-package com.example.watch.exception;
-
-import jakarta.validation.constraints.NotBlank;
-import org.springframework.web.bind.annotation.RequestBody;
-
-public class DuplicateResourceException extends Throwable {
-    public DuplicateResourceException(@NotBlank(message = "SKU is required") String s) {
-    }
-}
